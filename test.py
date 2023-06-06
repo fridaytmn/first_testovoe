ebcd265a5cbd9b54e599899026be0321a69cb0e9
Vlad Shvets <88648536+fridaytmn@users.noreply.github.com>
2023-06-06 09:27:59 +0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-
-if __name__ == '__main__':
-    server_address = ("", 8000)
-    httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-    httpd.serve_forever()
